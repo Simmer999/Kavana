d72b50b493090575aa424123feb9e22bd726424a
Simmer999 <fearlessleader5900@gmail.com>
2020-07-31 21:49:52 -0600

    Modify and add funtionality to 'biblio' files.

--- Kavana/Phil-links/app.js
+++ Kavana/Phil-links/app.js
@@ -1,17 +1,47 @@
-// let banner = document.getElementById("page-banner");
+const list = document.querySelector("#book-list ul")
 
-// let bookList = document.getElementById("book-list");
+//Delete books.
+list.addEventListener("click", function(e) {
+    if(e.target.className == "delete") {
+        const li = e.target.parentElement;
+        list.removeChild(li);
+    }
+});
 
-// document.getElementsByClassName("title");
+//Add book-list.
+const addForm = document.forms["add-book"];
+addForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const value = addForm.querySelector("input[type = 'text']").value;
 
-// document.getElementsByTagName("li");
+//Create elements.
+const li = document.createElement("li");
+const bookName = document.createElement("span");
+const deleteBtn = document.createElement("span");
 
-let titles = document.getElementsByClassName("title");
+//Add content.
+deleteBtn.textContent = "delete";
+bookName.textContent = value;
+
+//Add classes.
+bookName.classList.add("name");
+deleteBtn.classList.add("delete");
 
 
-// console.log(Array.isArray(titles));
-// console.log(Array.isArray(Array.from(titles)));
+//Append to document.
+li.appendChild(bookName);
+li.appendChild(deleteBtn);
+list.appendChild(li);
 
-Array.from(titles).forEach(function(item) {
-    console.log(item);
-})
+
+
+});
+
+const hideBox = document.querySelector("#hide");
+hideBox.addEventListener("change", function(e) {
+    if(hideBox.checked) {
+        list.style.display = "none";
+    } else { 
+        list.style.display = "initial";
+    }
+});
